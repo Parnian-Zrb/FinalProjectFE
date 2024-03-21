d171137a03b7e45047dab18a5469eae5dc628159
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-21 17:45:58 -0400

    about us content

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,5 +1,20 @@
+import "./AboutUs.css";
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <h1>AboutUs</h1>
+      <p>
+        Welcome to our food sharing recipe about page! Here, we believe in the
+        power of community, connection, and delicious food. Our platform is
+        dedicated to fostering a culture of sharing homemade recipes, culinary
+        experiences, and heartfelt stories. Whether you're a seasoned chef or a
+        kitchen newbie, you'll find a wealth of inspiration, creativity, and
+        mouthwatering dishes to explore and enjoy. Join us on this culinary
+        journey as we celebrate the joy of cooking and the beauty of sharing
+        food with those we love.
+      </p>
+    </div>
+  );
 };
 
 export default AboutUs;
