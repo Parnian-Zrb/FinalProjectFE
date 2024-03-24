8a97bdabf20e8a911deeacd9db24a7158dff6fa1
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-24 17:02:57 -0400

    fix

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -6,8 +6,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import "../pages/AboutUs.css";
 
-export default function ProfileCard({ name, image, bio }: any) {
->>>>>>> 1b9fea22cd9f96e76bf39652c19a960f7ce02f62
+export default function ProfileCard({ name, image, description, rating }: any) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -26,23 +25,10 @@ export default function ProfileCard({ name, image, bio }: any) {
           over 6,000 species, ranging across all continents except Antarctica
         </Typography>
         <Typography variant="body2" color="text.secondary">
-
           {rating}
         </Typography>
       </CardContent>
-      <CardActions>
-<<<<<<< HEAD
-=======
-=======
-        <Typography gutterBottom variant="h5" component="div">
-          {image}
-          {name}Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {bio}Lizards are a widespread group of squamate reptiles, with over
-          6,000 species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
+
       <CardActions>
         <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
