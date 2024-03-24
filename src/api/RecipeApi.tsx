fed574e83d3efe24f201813a95e888e0643aefb0
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-24 14:37:29 -0400

    //

--- src/api/RecipeApi.tsx
+++ src/api/RecipeApi.tsx
@@ -1,24 +1,23 @@
-//the same as api RecipeApi.tsx
-import axios from "axios";
-import { RecipeType } from "../types/recipe";
+// // the same as api RecipeApi.tsx
+// import axios from "axios";
+// import { RecipeType } from "../types/Recipe";
+// import { RecipeType } from "../types/Recipe";
 
-const API_URL = ""; //need to be completed after backend
+// // Fetch all Recipe
+// export const getAllRecipes = () => axios.get(API_URL);
 
-// Fetch all Recipe
-export const getAllRecipes = () => axios.get(API_URL);
+// // Create a newRecipe
+// export const createRecipe = (RecipeData: RecipeType) =>
+//   axios.post(API_URL, RecipeData);
 
-// Create a newRecipe
-export const createRecipe = (RecipeData: RecipeType) =>
-  axios.post(API_URL, RecipeData);
+// // Get a single Recipe
+// export const getRecipeById = (id: string) => axios.get(`${API_URL}/${id}`);
+// export const getRecipeByCategory = (userInput: string) =>
+//   axios.get(`${API_URL}/category/${userInput}`);
 
-// Get a single Recipe
-export const getRecipeById = (id: string) => axios.get(`${API_URL}/${id}`);
-export const getRecipeByCategory = (userInput: string) =>
-  axios.get(`${API_URL}/category/${userInput}`);
+// // Update a Recipe
+// export const updateRecipe = (id: string, RecipeData: RecipeType) =>
+//   axios.patch(`${API_URL}/${id}`, RecipeData);
 
-// Update a Recipe
-export const updateRecipe = (id: string, RecipeData: RecipeType) =>
-  axios.patch(`${API_URL}/${id}`, RecipeData);
-
-// Delete a Recipe
-export const deleteProduct = (id: string) => axios.delete(`${API_URL}/${id}`);
+// // Delete a Recipe
+// export const deleteProduct = (id: string) => axios.delete(`${API_URL}/${id}`);
