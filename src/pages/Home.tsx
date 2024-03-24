0dc8ad169449c4da3dc09e95d88b148ae17b74a0
Parnian Zarbafian <parnian.zarbafian@gmail.com>
2024-03-24 14:00:39 -0400

    pic an d desc

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Welcome to our website!</h1>
+
+      <img
+        src="https://images.unsplash.com/photo-1457666134378-6b77915bd5f2?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        width="1300px"
+        height="auto"
+      />
+      <p>
+        This is a food sharing recipe website where you can find recipes,
+        cooking tips and more. If you are new here, please take some time to
+        explore the site and see what we have for you. You can also search for
+        specific dishes or browse through our categories to discover something
+        that suits your taste. We hope you enjoy your stay with us!
+      </p>
+    </>
+  );
 };
 
 export default Home;
