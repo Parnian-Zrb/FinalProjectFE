d9de8b809c2b53bd541f4c881d14d5f2a8df9ac0
Shelly_Wu <shellywu37@gmail.com>
2024-03-25 17:42:54 -0400

    fetch publish recipe data

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,6 +1,26 @@
 import RecipeCard from "../components/RecipeCard";
+import { useFavoriteStore } from "../store/useFavouriteStore";
+import { useState, useEffect } from "react";
+import { RecipeType } from "../types/Recipe";
 
 const UserProfile = () => {
+  const { favorites } = useFavoriteStore();
+  const [userRecipes, addUserRecipes] = useState<RecipeType[]>([]);
+
+  useEffect(() => {
+    const fetchUserRecipes = async () => {
+      try {
+        const response = await fetch("/user/:id/recipes");
+        const data = await response.json();
+        addUserRecipes(data);
+      } catch (error) {
+        console.error("Failed to fetch user recipes:", error);
+      }
+    };
+
+    fetchUserRecipes();
+  }, []);
+
   return (
     <div>
       <h2>UserProfile</h2>
@@ -9,12 +29,15 @@ const UserProfile = () => {
       </section>
       <section>
         <h3>Favourited Recipe</h3>
-
-        <RecipeCard name={""} image={""} description={""} rating={""} />
+        {favorites.map((recipe) => (
+          <RecipeCard key={recipe._id} recipe={recipe} />
+        ))}
       </section>
       <section>
         <h3>Publish Recipe</h3>
-        <RecipeCard name={""} image={""} description={""} rating={""} />
+        {userRecipes.map((recipe) => (
+          <RecipeCard key={recipe._id} recipe={recipe} />
+        ))}
       </section>
     </div>
   );
