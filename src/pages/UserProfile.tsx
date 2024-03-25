69d26cbe33dd753164570110383adeea7a344aff
Shelly_Wu <shellywu37@gmail.com>
2024-03-25 10:13:14 -0400

    update userprofile

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import RecipeCard from "../components/RecipeCard";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  return (
+    <div>
+      <h2>UserProfile</h2>
+      <section>
+        <h3>Meal Plan</h3>
+      </section>
+      <section>
+        <h3>Favourited Recipe</h3>
+
+        <RecipeCard name={""} image={""} description={""} rating={""} />
+      </section>
+      <section>
+        <h3>Publish Recipe</h3>
+        <RecipeCard name={""} image={""} description={""} rating={""} />
+      </section>
+    </div>
+  );
 };
 
 export default UserProfile;
