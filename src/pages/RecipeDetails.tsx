1214cf2745756e0aef53fc045d41083189a54bd2
Shelly_Wu <shellywu37@gmail.com>
2024-03-26 10:00:25 -0400

    update product detail with  backend data

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -1,128 +1,162 @@
-import './RecipeDetails.css';
+import "./RecipeDetails.css";
+import { RecipeType } from "../types/Recipe";
+import { useEffect, useState } from "react";
+import { getRecipeById } from "../api/recipeApi";
+import { useParams } from "react-router-dom";
 
-interface RecipeDetailsProps {
-  _id: string;
-  name: string;
-  introduction: string;
-  preparationTime: number;
-  servings: number;
-  image: string;
-  ingredients: string[];
-  instructions: string[];
-  difficulty: string[];
-  createdAt: Date;
-  userId: string;
-  mealPlan: string;
-  category: string;
-}
+// interface RecipeDetailsProps {
+//   _id: string;
+//   name: string;
+//   introduction: string;
+//   preparationTime: number;
+//   servings: number;
+//   image: string;
+//   ingredients: string[];
+//   instructions: string[];
+//   difficulty: string[];
+//   createdAt: Date;
+//   userId: string;
+//   mealPlan: string;
+//   category: string;
+// }
 
+// const RecipeDetails = ({
+// /*   _id, */
+//   name,
+//   introduction,
+//   preparationTime,
+//   servings,
+//   image,
+//   ingredients,
+//   instructions,
+//   difficulty,
+//  /*  createdAt,
+//   userId, */
+//   mealPlan,
+//   category,
 
+// } : RecipeDetailsProps) => {
 
-const RecipeDetails = ({
-/*   _id, */
-  name,
-  introduction,
-  preparationTime,
-  servings,
-  image,
-  ingredients,
-  instructions,
-  difficulty,
- /*  createdAt,
-  userId, */
-  mealPlan,
-  category,
+const RecipeDetail = () => {
+  const { id = "" } = useParams();
+  const [recipe, setRecipe] = useState<RecipeType>();
 
-} : RecipeDetailsProps) => {
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        if (id) {
+          const response = await getRecipeById(id);
+          // console.log(response);
+          setRecipe(response.data);
+        } else {
+          console.error("Product ID is undefined");
+        }
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
 
+    fetchRecipe();
+  }, [id]);
 
-   const handleSubmitComment = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmitComment = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Handle submitting comment
-  }; 
- 
+  };
+
   return (
     <div className="recipe-details">
       <header>
-        <h1>Food Name:{name}</h1>
+        <h1>Food Name:{recipe?.name}</h1>
         <p> Navigation Links, Save/Favorite Button, Share Buttons </p>
-     </header>
-      
+      </header>
+
       <main>
-      <div className="recipe-image">
-        <img src='https://media.istockphoto.com/id/1156304531/photo/hands-with-the-phone-close-up-pictures-of-food.jpg?s=1024x1024&w=is&k=20&c=eGTWUqsEBCkkVhliE0QVxZ5IHdwJJwWMwur89LEmizg=' alt="Recipe Image" />
-      </div>
-      
-       <table className='table'>
-        <td className='td'>
-            <div className="recipe-info" >
-              <tr ><h2>Ingredients:{ingredients}</h2></tr>
-              <tr ><h2>preparation Time: {preparationTime}</h2></tr> 
-              <tr ><h2>Introduction: {introduction}</h2></tr> 
-             </div>
-        </td>
-        <td className='td'>
-            <div>
-              <tr><h2>Difficulty:{difficulty}</h2></tr>
-              <tr><h2>Category: {category} </h2> </tr>
-              <tr><h2>Servings for:{servings} </h2></tr>
+        <div className="recipe-image">
+          <img src={recipe?.image} alt="Recipe Image" />
+        </div>
+
+        <table className="table">
+          <td className="td">
+            <div className="recipe-info">
+              <tr>
+                <h2>Ingredients:{recipe?.ingredients}</h2>
+              </tr>
+              <tr>
+                <h2>preparation Time: {recipe?.preparationTime}</h2>
+              </tr>
+              <tr>
+                <h2>Introduction: {recipe?.description}</h2>
+              </tr>
             </div>
-        </td>
+          </td>
+          <td className="td">
+            <div>
+              <tr>
+                <h2>Difficulty:{recipe?.difficulty}</h2>
+              </tr>
+              <tr>
+                <h2>Category: {recipe?.category} </h2>{" "}
+              </tr>
+              <tr>
+                <h2>Servings for:{recipe?.servings} </h2>
+              </tr>
+            </div>
+          </td>
         </table>
       </main>
 
       <section className="Instructions">
         <h2>Instructions:</h2>
-        <p>{instructions}</p>
+        <p>{recipe?.instructions}</p>
       </section>
-      
-    <div>
-      <section className="meal-plan">
-          <h2>Meal Plan: {mealPlan}</h2>
-           {mealPlan}
-       </section>
+
+      <div>
+        <section className="meal-plan">
+          <h2>Meal Plan: {recipe?.mealPlan}</h2>
+          {recipe?.mealPlan}
+        </section>
       </div>
 
-    <table>
+      <table>
+        <td className="td">
+          <section>
+            <div className="comment_list">
+              <h2> Comment list </h2>
 
-    <td className='td'>
-      <section>
-       <div className="comment_list">
-       <h2> Comment list </h2>
-       
-        <li> First Comment </li>
-        <li> Second Comment  </li>
-        <li> Third Comment </li>
-        <li> Forth Comment </li>
-        </div>
-        </section>
-      </td>
-      
+              <li> First Comment </li>
+              <li> Second Comment </li>
+              <li> Third Comment </li>
+              <li> Forth Comment </li>
+            </div>
+          </section>
+        </td>
 
-      <td className='td'>
-         <section >
-           <div className="comments">
-         
-            <h2>Comments:</h2>
-            <form onSubmit={handleSubmitComment}>
-              <h2>Name </h2>
-            <input type="name" placeholder="Name" />
-            <h2>Email</h2>
+        <td className="td">
+          <section>
+            <div className="comments">
+              <h2>Comments:</h2>
+              <form onSubmit={handleSubmitComment}>
+                <h2>Name </h2>
+                <input type="name" placeholder="Name" />
+                <h2>Email</h2>
 
-            <input type="Email" placeholder="Email" />
-            <h2>Comments</h2>
+                <input type="Email" placeholder="Email" />
+                <h2>Comments</h2>
 
-            <input type="text" placeholder="Add a comment" />
-            <button className="comments button" type="submit">Submit</button>
-           </form>
-          </div>
-        </section>
-      </td>
+                <input type="text" placeholder="Add a comment" />
+                <button className="comments button" type="submit">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </section>
+        </td>
       </table>
 
       <div className="scroll-menu">
         <h2>Related Recipe-same category </h2>
-        Here are :A scroll-menu of 5  recipes in this category
+        Here are :A scroll-menu of 5 recipes in this category
         {/* <ul>
           {recipeTitles.map((title, index) => (
             <li key={index}>{title}</li>
@@ -139,8 +173,7 @@ const RecipeDetails = ({
         </ul> */}
       </div>
 
-
-    <footer>
+      <footer>
         <p>Copyright © {new Date().getFullYear()} FoodSharing Group</p>
         <nav>
           <a href="#">Privacy Policy</a>
@@ -148,9 +181,8 @@ const RecipeDetails = ({
           <a href="#">Contact Us</a>
         </nav>
       </footer>
-
     </div>
   );
 };
 
-export default RecipeDetails;
+export default RecipeDetail;
