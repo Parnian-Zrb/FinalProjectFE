2a6e2e387dbe533958f6fc738fc7839e4122c37a
Shelly_Wu <shellywu37@gmail.com>
2024-03-25 22:32:56 -0400

    fetch one user data for profile

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,24 +1,27 @@
 import axios from "axios";
-import { RecipeType } from "../types/Recipe";
+// import { RecipeType } from "../types/Recipe";
 // the same as api RecipeApi.tsx
 
 const API_URL = `${import.meta.env.VITE_BASE_URL}/user`;
-console.log(API_URL);
+// console.log(API_URL);
+
+export const getUserById = (id: string) => axios.get(`${API_URL}/${id}`);
+
 // Fetch all Recipe
-export const getAllRecipes = () => axios.get(`${API_URL} `);
+// export const getAllRecipes = () => axios.get(`${API_URL} `);
 
-// Create a newRecipe
-export const createRecipe = (RecipeData: RecipeType) =>
-  axios.post(API_URL, RecipeData);
+// // Create a newRecipe
+// export const createRecipe = (RecipeData: RecipeType) =>
+//   axios.post(API_URL, RecipeData);
 
-// Get a single Recipe
-export const getRecipeById = (id: string) => axios.get(`${API_URL}/${id}`);
-export const getRecipeByCategory = (userInput: string) =>
-  axios.get(`${API_URL}/category/${userInput}`);
+// // Get a single Recipe
+// export const getRecipeById = (id: string) => axios.get(`${API_URL}/${id}`);
+// export const getRecipeByCategory = (userInput: string) =>
+//   axios.get(`${API_URL}/category/${userInput}`);
 
-// Update a Recipe
-export const updateRecipe = (id: string, RecipeData: RecipeType) =>
-  axios.patch(`${API_URL}/${id}`, RecipeData);
+// // Update a Recipe
+// export const updateRecipe = (id: string, RecipeData: RecipeType) =>
+//   axios.patch(`${API_URL}/${id}`, RecipeData);
 
-// Delete a Recipe
-export const deleteProduct = (id: string) => axios.delete(`${API_URL}/${id}`);
+// // Delete a Recipe
+// export const deleteProduct = (id: string) => axios.delete(`${API_URL}/${id}`);
